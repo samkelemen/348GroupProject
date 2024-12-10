a79adb15f190f7efaf30acc6f027581185244e86
Sam Kelemen <86901068+samkelemen@users.noreply.github.com>
2024-12-10 11:58:06 -0600

    Update main.cpp to create tree with 'parser'

--- main.cpp
+++ main.cpp
@@ -3,16 +3,15 @@
 */
 
 #include "user_interface.h"
-//#include "parser.h"
-//#include "calculator.h"
-
-using  std::string;
+#include "node.hpp"
+#include "parser.hpp"
+using std::string;
 
 
 int main() {
     while (true) {
         string input = input();
-        // class compute_graph = parser(string);
+        Node* compute_tree = Parser::create_tree(string expression);
         // float result = calculator(compute_graph);
         // output_result(result);
     }
